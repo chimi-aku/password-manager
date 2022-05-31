93be975dba910829dc1172deadf0543cd321a241
Kacper <66553710+kacperchrost@users.noreply.github.com>
2022-05-31 17:18:27 +0200

    Początek logowania

---
+++ src/main/java/com/example/passwordmanager/FileHandler.java
@@ -0,0 +1,50 @@
+package com.example.passwordmanager;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class FileHandler {
+    private static BufferedImage img;
+
+
+    public static BufferedImage LoadImage(Stage stage){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Image Files", "*.jpg", "*.png"));
+
+        File file = fileChooser.showOpenDialog(stage);
+
+        try {
+            img = ImageIO.read(file);
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return img;
+    }
+
+    public static Image convertToFxImage(BufferedImage image) {
+        // convert Buffered Img to Image
+
+        WritableImage wr = null;
+        if (image != null) {
+            wr = new WritableImage(image.getWidth(), image.getHeight());
+            PixelWriter pw = wr.getPixelWriter();
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = 0; y < image.getHeight(); y++) {
+                    pw.setArgb(x, y, image.getRGB(x, y));
+                }
+            }
+        }
+        return new ImageView(wr).getImage();
+    }
+}
--- src/main/java/com/example/passwordmanager/HelloController.java
+++ src/main/java/com/example/passwordmanager/HelloController.java
@@ -1,14 +1,45 @@
 package com.example.passwordmanager;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.awt.image.BufferedImage;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    @FXML private Button loadImageBtn;
+    private BufferedImage originalImage;
+    private ImageView originalImageView;
+    @FXML private RadioButton medianFilter;
+    @FXML private RadioButton kuwaharaFilter;
+    @FXML private RadioButton mdbutmfFilter;
+    @FXML private RadioButton medianmedianFilter;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    @FXML protected void onLoadImageBtnClick()
+    {
+        Stage thisStage = (Stage) loadImageBtn.getScene().getWindow();
+        originalImage = FileHandler.LoadImage(thisStage);
+        originalImageView.setImage(FileHandler.convertToFxImage(originalImage));
+    }
+
+    @FXML protected void onLogInBtnClick()
+    {
+        if(medianFilter.isSelected()){
+
+        }
+        if(kuwaharaFilter.isSelected()){
+
+        }
+        if(mdbutmfFilter.isSelected()){
+
+        }
+        if(medianmedianFilter.isSelected()){
+
+        }
+
     }
 }
