19df9ab4c154b1a79e6a93f26ca65d1f2c7ebb17
Fisto <Fisto@DESKTOP-8N013EB>
2022-06-23 21:51:19 +0200

    jeeee

--- src/main/java/com/example/passwordmanager/ManagerController.java
+++ src/main/java/com/example/passwordmanager/ManagerController.java
@@ -72,14 +72,20 @@ public class ManagerController {
 
     }
     public void AddBtn(ActionEvent actionEvent) {
+
         Dialog<Results> dialog = new Dialog<>();
         dialog.setTitle("Dodaj");
         dialog.setHeaderText("Podaj link i hasło");
         DialogPane dialogPane = dialog.getDialogPane();
         dialogPane.getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
-        TextField urlField = new TextField("Link");
-        TextField passField = new TextField("Hasło");
+
+        TextField urlField = new TextField();
+        urlField.setPromptText("Link");
+        TextField passField = new TextField();
+        passField.setPromptText("Hasło");
+
         dialogPane.setContent(new VBox(8, urlField, passField));
+
         dialog.setResultConverter((ButtonType button) -> {
             if (button == ButtonType.OK) {
                 return new Results(urlField.getText(),
@@ -87,34 +93,95 @@ public class ManagerController {
             }
             return null;
         });
+
         Optional<Results> optionalResult = dialog.showAndWait();
         optionalResult.ifPresent((Results results) -> {
+
+            list.getItems().add(list.getItems().size(),new String[]{results.url,results.pass});
+            list.refresh();
+
             new Thread(() -> {
                 try(var conn = DManager.connect()){
                     Statement statement = conn.createStatement();
-                    statement.executeUpdate("insert into public.\"ListOfPasswords\"('Link','Pass','IdUser') values("+results.url+","+results.pass+","+id+")");
+                    statement.executeUpdate("insert into public.\"ListOfPasswords\"values(nextval('listseq'),'"+results.url+"','"+results.pass+"',"+id+")");
                 } catch (SQLException e) {
                     System.out.println(e);
                 }
 
             }).start();
 
-            list.getItems().add(list.getItems().size(),new String[]{results.url,results.pass});
-            list.refresh();
+
         });
 
 
     }
 
     public void DeleteBtn(ActionEvent actionEvent) {
+
+
         if(list.getSelectionModel().getSelectedItem() != null){
             list.getItems().remove(list.getSelectionModel().getSelectedItem());
             list.refresh();
         }
+        String[] tmp = list.getSelectionModel().getSelectedItem();
+        new Thread(() -> {
+            try(var conn = DManager.connect()){
+                Statement statement = conn.createStatement();
+                statement.executeUpdate("delete from public.\"ListOfPasswords\" lop where lop.\"Link\" = '"+ tmp[0]+ "' and lop.\"Pass\" ='"+tmp[1]+"'");
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+
+        }).start();
+
 
     }
 
     public void EditBtn(ActionEvent actionEvent) {
+        Dialog<Results> dialog = new Dialog<>();
+        dialog.setTitle("Edytuj");
+        dialog.setHeaderText("Podaj link i hasło");
+        DialogPane dialogPane = dialog.getDialogPane();
+        dialogPane.getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+        String[] toUp = list.getSelectionModel().getSelectedItem();
+        TextField urlField = new TextField(toUp[0]);
+        urlField.setPromptText("Link");
+        TextField passField = new TextField(toUp[1]);
+        passField.setPromptText("Hasło");
+
+        dialogPane.setContent(new VBox(8, urlField, passField));
+
+        dialog.setResultConverter((ButtonType button) -> {
+            if (button == ButtonType.OK) {
+                return new Results(urlField.getText(),
+                        passField.getText());
+            }
+            return null;
+        });
+
+        Optional<Results> optionalResult = dialog.showAndWait();
+        optionalResult.ifPresent((Results results) -> {
+
+            String[] tmp = {results.url,results.pass};
+            if(list.getSelectionModel().getSelectedItem() != null){
+
+                list.getItems().add(list.getSelectionModel().getSelectedIndex(),tmp);
+                list.getItems().remove(list.getSelectionModel().getSelectedIndex());
+                list.refresh();
+            }
+
+            new Thread(() -> {
+                try(var conn = DManager.connect()){
+                    Statement statement = conn.createStatement();
+                    statement.executeUpdate("update public.\"ListOfPasswords\" lop set \"Link\" = '"+tmp[0]+"',\"Pass\" = '"+ tmp[1]+"'  where lop.\"Link\" = '"+ toUp[0]+ "' and lop.\"Pass\" ='"+toUp[1]+"'");
+                } catch (SQLException e) {
+                    System.out.println(e);
+                }
+
+            }).start();
+
+
+        });
 
     }
 
